234b971012b159a83dc2cc2326504275eb32f443
Adria <adria.cortes@estudiant.enti.cat>
2025-11-19 12:25:39 +0100

    Eliminado script E33, añadido ejercicios

--- Assets/E05/E05.cs
+++ Assets/E05/E05.cs
@@ -1,20 +1,19 @@
 using UnityEngine;
 
-public class E33 : MonoBehaviour
+public class E05 : MonoBehaviour
 {
+    public int numberA;
+    public int numberB;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        Debug.Log(numberA + numberB);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float speed = 1;
-        Vector3 dir = new Vector3(1, 1, 0);
-        Quaternion rdir = Quaternion.Euler(0, 0, 1);
-
-        transform.position += dir * speed * Time.deltaTime;
+        
     }
 }
